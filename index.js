f7682a394dcb0e102bfc060d6f1304706d395e7d
Michael Eden <themichaeleden@gmail.com>
2025-04-14 16:07:18 -0400

    add ssh deploy key

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function run() {
       }
     }
     core.exportVariable('NIXBUILD_TOKEN', core.getInput('nixbuild_token'));
+    core.exportVariable('SSH_DEPLOY_KEY', core.getInput('ssh_deploy_key'));
     await exec.exec(path.resolve(__dirname, 'nixbuild-action.sh'), [JSON.stringify(inputs)]);
   } catch (error) {
     core.setFailed(error.message);
